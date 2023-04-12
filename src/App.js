0ca6bf386ccc2bb34fa14a10dbd620cf9ad70bb9
Gabriel Rodriguez <gfrodriguezclon@outlook.com>
2023-04-12 12:51:58 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Gallery from "./gallery.js";
 import Animation from "./animation.js";
 
--- src/animation.js
+++ src/animation.js
@@ -1,3 +1,4 @@
+import React from "react";
 export default function Animation() {
   return (
     <div className="view">
